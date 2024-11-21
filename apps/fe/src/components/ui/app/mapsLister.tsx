444339609c411ef9e47089d3cba269e3e70e4743
Karthik150502 <karthikrdycool@gmail.com>
2024-11-21 21:40:36 +0530

    Latest, edited the map

--- apps/fe/src/components/ui/app/mapsLister.tsx
+++ apps/fe/src/components/ui/app/mapsLister.tsx
@@ -3,6 +3,15 @@ import React, { useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import { getMaps } from '@/actions/getMaps'
 import MapCard from './mapCard'
+
+type SingleMapType = {
+    id: string
+    name: string,
+    width: number,
+    height: number,
+    thumbnail?: string
+}
+
 export default function MapsLister({ setMapId }: { setMapId: (val: string) => void }) {
 
 
@@ -26,7 +35,7 @@ export default function MapsLister({ setMapId }: { setMapId: (val: string) => vo
             <div className='w-full h-[350px] grid grid-cols-2 gap-y-4 place-content-start place-items-center overflow-auto no-scrollbar py-4'>
                 {
                     isPending ? <p>Loading....</p> : (
-                        data?.map((map) => {
+                        data?.map((map: SingleMapType) => {
                             return <MapCard id={map.id} onSelect={onSelect} selected={selectedMap === map.id} key={map.id} name={map.name} height={map.height} width={map.width} />
                         })
                     )
